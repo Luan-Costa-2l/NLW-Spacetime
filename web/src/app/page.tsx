7d582408de04b3de2690643ec478ac73aeba5835
Luan Cordeiro <luam.lc19@gmail.com>
2023-05-17 22:04:42 -0300

    feat(web): start right side and fix eslint config

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,8 +1,20 @@
 export default function Home() {
   return (
-    <div className="bg-zinc-950 h-screen text-white">
-      <h1 className="text-center uppercase">Hello World</h1>
-      <h1 className="text-center uppercase font-alt">Hello World2</h1>
-    </div>
+    <main className="grid min-h-screen grid-cols-2">
+      {/* LeftSide */}
+      <div></div>
+
+      {/* RitghSide */}
+      <div className="flex flex-col p-16">
+        <div className="flex flex-1 items-center justify-center">
+          <p className="w-[360px] text-center leading-relaxed">
+            Você ainda não registrou nenhuma lembrança, comece a{' '}
+            <a href="" className="underline hover:text-gray-50">
+              criar agora!
+            </a>
+          </p>
+        </div>
+      </div>
+    </main>
   )
 }
