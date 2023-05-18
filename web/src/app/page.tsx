f1f864bfd2e7be2dcf8a134eb5d44d65b1f6ce24
Luan Cordeiro <luam.lc19@gmail.com>
2023-05-17 21:11:27 -0300

    style(web): configure fonts

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,5 +1,8 @@
 export default function Home() {
   return (
-    <h1 className="text-center uppercase">Hello World</h1>
+    <div className="bg-zinc-950 h-screen text-white">
+      <h1 className="text-center uppercase">Hello World</h1>
+      <h1 className="text-center uppercase font-alt">Hello World2</h1>
+    </div>
   )
 }
