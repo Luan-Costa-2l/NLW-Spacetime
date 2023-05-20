cd49708aeeffc6aef9098c5cd7e141287dbadedc
Luan Cordeiro <luam.lc19@gmail.com>
2023-05-20 16:06:54 -0300

    refactor(web): componentize the page/code

---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,15 @@
+export function Copyright() {
+  return (
+    <div className="text-sm">
+      Feito com 💜 no NLW da{' '}
+      <a
+        className="underline transition-colors hover:text-gray-50"
+        href="https://rocketseat.com.br"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Rocketseat
+      </a>
+    </div>
+  )
+}
