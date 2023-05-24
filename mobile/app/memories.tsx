db6914f4420d4177f42f095e1d5c1e5ba4c752fb
Luan Cordeiro <luam.lc19@gmail.com>
2023-05-24 10:03:30 -0300

    feat(mobile): create and use layout of memory list

--- mobile/app/memories.tsx
+++ mobile/app/memories.tsx
@@ -1,12 +1,15 @@
-import { View, TouchableOpacity, ScrollView } from 'react-native'
+import { View, Text, TouchableOpacity, ScrollView, Image } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
 import { Link, useRouter } from 'expo-router'
 import Icon from '@expo/vector-icons/Feather'
 import * as SecureStore from 'expo-secure-store'
 import { useEffect, useState } from 'react'
+import dayjs from 'dayjs'
+import ptBr from 'dayjs/locale/pt-br'
 import { api } from '../src/lib/api'
 
+dayjs.locale(ptBr)
 
 interface MemoriesType {
   coverUrl: string
@@ -42,10 +45,10 @@ export default function Memories() {
 
   return (
     <ScrollView
-      className="flex-1 px-8"
+      className="flex-1"
       contentContainerStyle={{ paddingTop: top, paddingBottom: bottom }}
     >
-      <View className="mt-4 flex-row items-center justify-between">
+      <View className="mt-4 flex-row items-center justify-between px-8">
         <NLWLogo width={190} />
         <View className="flex-row gap-2">
           <TouchableOpacity
@@ -61,6 +64,39 @@ export default function Memories() {
             </TouchableOpacity>
           </Link>
         </View>
+      </View>
+
+      <View className="mt-6 space-y-10">
+        {memories.map((memory) => (
+          <View className="space-y-4" key={memory.id}>
+            <View className="flex-row items-center gap-2">
+              <View className="h-px w-5 bg-gray-50"></View>
+              <Text className="font-body text-xs text-gray-100">
+                {dayjs(memory.createdAt).format('D[ de ]MMMM[ de ]YYYY')}
+              </Text>
+            </View>
+            <View className="space-y-4 px-8">
+              <Image
+                source={{
+                  uri: memory.coverUrl,
+                }}
+                alt=""
+                className="aspect-video w-full rounded-lg"
+              />
+              <Text className="text-justify font-body text-base leading-relaxed text-gray-100">
+                {memory.excerpt}
+              </Text>
+              <Link href="/memories/id" asChild>
+                <TouchableOpacity className="flex-row items-center gap-2">
+                  <Text className="font-body text-sm text-gray-200">
+                    Ler mais
+                  </Text>
+                  <Icon name="arrow-right" size={16} color="#9e9ea0" />
+                </TouchableOpacity>
+              </Link>
+            </View>
+          </View>
+        ))}
       </View>
     </ScrollView>
   )
